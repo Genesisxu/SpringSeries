6010a91bdc8b19065617098255a3169554126a96
Genesis <genesis2wx@gmail.com>
2016-09-28 10:09:15 +0800

    delete useless file

--- SpringBoot-2/src/main/java/com/god/Application.java
+++ SpringBoot-2/src/main/java/com/god/Application.java
@@ -10,22 +10,3 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 }
-//@Configuration
-//@ComponentScan("com.god")
-//@EnableAutoConfiguration
-//// @SpringBootApplication
-//public class Application {
-//
-//	public static void main(String[] args) {
-//		SpringApplication app = new SpringApplication(Application.class);
-//		app.setWebEnvironment(true);
-////		app.setShowBanner(false);
-//
-//		Set<Object> set = new HashSet<Object>();
-//		// set.add("classpath:applicationContext.xml");
-//		app.setSources(set);
-//		app.run(args);
-//
-//	}
-//
-//}
--- SpringBoot-2/src/main/webapp/index.jsp
+++
@@ -1,6 +0,0 @@
-<%@ page language="java" import="java.util.*" pageEncoding="utf-8"%>
-<%
-    String path = request.getContextPath();
-    String basePath = request.getScheme()+"://"+request.getServerName()+":"+request.getServerPort()+path+"/";
-    response.sendRedirect(basePath + "sys/login_toLoginUI.action");
-%>
